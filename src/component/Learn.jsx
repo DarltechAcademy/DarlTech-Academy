b59387748f307697b1f442272582afb1a50e7da3
DarlTech <darltechacademy@gmail.com>
2026-02-26 05:46:33 +0100

    Simplify the message about coding experience
    Removed introductory sentence about experience.

--- src/component/Learn.jsx
+++ src/component/Learn.jsx
@@ -8,7 +8,7 @@ export default function Learn() {
       </div>
       <div>
         <h1>CODING EXPERIENCE OPTIONAL;<br />DETERMINATION REQUIRED</h1>
-        <p>Thanks to over 7 years of building software across mobile, web and desktop apps, we have what it takes to transform you into a Tech Warrior. We know that if you are committed to this journey, you will be able to succeed. That’s why our admissions process assesses only your determination – not your coding ability.</p>
+        <p>We have what it takes to transform you into a Tech Warrior. We know that if you are committed to this journey, you will be able to succeed. That’s why our admissions process assesses only your determination – not your coding ability.</p>
       </div>
     </div>
   )
